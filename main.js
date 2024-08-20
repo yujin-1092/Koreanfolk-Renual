d2e724a966e83e059146fa92ea18abe737095877
yujin-1092 <kunche1092@gmail.com>
2024-08-20 09:30:22 +0900

    스크롤 기능 구현 시도

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let currentIdx = 0;
 let pager = slideWrapper.querySelector('.pager')
 let pagerHTML ='';
 
+// main banner 페이저
 slide.forEach((item,idx)=>{
   item.style.left = `${idx *100}%`;
   pagerHTML = pagerHTML + `<a href="#">${idx}</a>`;
@@ -14,3 +15,29 @@ slide.forEach((item,idx)=>{
 pager.innerHTML = pagerHTML;
 let pagerBtn = pager.querySelectorAll('a');
 console.log(pagerHTML);
+
+
+
+// 스크롤
+const scrollTopBtn = document.querySelector('.scroll');
+const arrowIcon = scrollTopBtn.querySelector('.fa-chevron-down');
+
+// 스크롤 감지
+window.addEventListener('scroll',()=>{
+  if (window.scrollY > 100) {
+    scrollTopBtn.classList.add('topMode');
+    scrollTopBtn.innerHTML = 'Top <i class="fa-solid fa-chevron-down"></i>';
+  } else {
+    scrollTopBtn.classList.remove('topMode');
+    scrollTopBtn.innerHTML = 'Scroll <i class="fa-solid fa-chevron-down"></i>';
+  }
+});
+
+// TOP이 클릭됐을 때 상단으로 스크롤
+scrollTopBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+});
